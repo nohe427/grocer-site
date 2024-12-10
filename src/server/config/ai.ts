7af50c5ee7464fb4a8c76e3ee25faedc9dfbacf2
Alexander Nohe <nohe@google.com>
2024-12-10 13:24:05 -0800

    Update auth to include project name

--- src/server/config/ai.ts
+++ src/server/config/ai.ts
@@ -6,7 +6,7 @@ export const ai = genkit({
       // Load the Vertex AI plugin. You can optionally specify your project ID
       // by passing in a config object; if you don't, the Vertex AI plugin uses
       // the value from the GCLOUD_PROJECT environment variable.
-      vertexAI({ location: 'us-central1' }),
+      vertexAI({ location: 'us-central1', projectId: 'lon-next' }),
     ],
     // Log debug output to tbe console.
     // logLevel: 'debug', -- Where is this? TODO:@nohe
