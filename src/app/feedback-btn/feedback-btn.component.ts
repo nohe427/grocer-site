2d5559a3bbf4f1dc73aa0e57fe3c0208ae760c10
Jeff Huleatt <3759507+jhuleatt@users.noreply.github.com>
2024-12-19 14:08:05 -0500

    get values from the feedback form

--- src/app/feedback-btn/feedback-btn.component.ts
+++ src/app/feedback-btn/feedback-btn.component.ts
@@ -9,15 +9,28 @@ import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 })
 export class FeedbackBtnComponent {
   @ViewChild('feedbackDialog') feedbackDialog?: ElementRef<HTMLDialogElement>;
-
+  @ViewChild('noRadio') noRadio?: ElementRef<HTMLInputElement>;
+  @ViewChild('yesRadio') yesRadio?: ElementRef<HTMLInputElement>;
+  @ViewChild('freeFromInputBox') freeFromInputBox?: ElementRef<HTMLTextAreaElement>;
+  
   @Input() traceId: string = ""
   @Input() spanId: string = "";
   @Input() flowName: string = "";
 
   show = true;
 
-  // // Uncomment this to open the popover by default to work on styling
-  // ngAfterViewInit() {
-  //   this.feedbackDialog?.nativeElement.showPopover();
-  // }
+  submitFeedback(event: Event) {
+    event.preventDefault();
+
+    const feedbackResponse = {
+      traceId: this.traceId,
+      spanId: this.spanId,
+      flowName: this.flowName,
+      helpful: this.yesRadio?.nativeElement.checked || false,
+      freeInput: this.freeFromInputBox?.nativeElement.value || ""
+    }
+
+    // TODO(nohe): send this somewhere...
+    console.log(feedbackResponse);
+  }
 }
